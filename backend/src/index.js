93f00d8c7670ac2dfceba7186b17848cfee10aaa
Abhii200 <saiabhinaygedela@gmail.com>
2024-09-01 12:17:45 +0530

    end points

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,26 @@
-const { Client } = require('pg');
+// src/index.js
 
-const client = new Client({
-    connectionString: 'postgres://postgres:password@localhost:5430/hosp_db' 
+require('dotenv').config();
+const express = require('express');
+const setupDatabase = require('./setUpDatabase');
+const hospitalRoutes = require('./routes/hospital');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Route to setup the database
+// app.get('/setup-database', async (req, res) => {
+//     try {
+//         await setupDatabase();
+//         res.send('Database setup complete!');
+//     } catch (error) {
+//         res.status(500).send('Error setting up the database.');
+//     }
+// });
+
+// Use hospital routes
+app.use('/api', hospitalRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
-
-client.connect()
-    .then(() => {
-        console.log('Connected to PostgreSQL!'); 
-    })
-    
